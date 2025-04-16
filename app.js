6fbae9092078d30b4f4fb84093e00c4f554e4032
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-04-16 15:22:43 +0530

    creating boiler plate

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate'); 
+const methodOverride = require('method-override');
+
+
+
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({extended:true}) );
-const methodOverride = require("method-override");
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+
+
 //momgodb connection string
 const MONGOURL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -26,6 +35,9 @@ async function main() {
     
 }
 
+
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 } );
@@ -34,19 +46,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// app.get('/testListing', async (req, res) => {
-//     let sampleListing = new Listing({
-//         title: 'Eagle Palace',
-//         description: 'Best Hotel in Pachora',
-//         price: 100,
-//         location: 'Jalgoan',
-//         country: 'India'
-//     });
 
-//     await sampleListing.save();
-//     console.log('Listing saved:', sampleListing);
-//     res.send('Listing saved!');
-// });
+
+
 
 //Index ROute
 app.get('/listings', async (req, res) => {
@@ -55,19 +57,23 @@ app.get('/listings', async (req, res) => {
     res.render("listings/index.ejs", { allListings });
   
 });
+
+//Create Route
 app.post("/listings", async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
   });
 
-  //New Route
+//New Route
 app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
   });
+
+//Show Route
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
-    id = id.trim(); // Remove leading/trailing spaces
+    id = id.trim(); 
     try {
         const listing = await Listing.findById(id);
         if (!listing) {
@@ -80,9 +86,6 @@ app.get("/listings/:id", async (req, res) => {
     }
 });
 
-//Create Route
-
-  
   //Edit Route
   app.get("/listings/:id/edit", async (req, res) => {
     let { id } = req.params;
