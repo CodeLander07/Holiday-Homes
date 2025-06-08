00a81617b34b0345d915b5db9b23b7b9ea237fdf
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-06-08 13:10:27 +0530

    feat: update navbar and styling, replace Airbnb icon with custom SVG

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
